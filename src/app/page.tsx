f79d4a72c2a1ce2989ed5eebaf483a7040d96186
danieltsega <danieltsega6658@gmail.com>
2024-10-28 15:08:04 +0300

    feat: add bio component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
+import Bio from "@/components/bio";
 import Navbar from "@/components/navbar";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="md:max-w-3xl mx-auto md:mt-8">
       <Navbar></Navbar>
-      Hello World!
-      <Button variant={"secondary"}>Button</Button>
+      <Bio />
     </div>
   );
 }

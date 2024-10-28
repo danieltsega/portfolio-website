e3fa5961d1a1a13e06726e2ae4710f7129f2f517
danieltsega <danieltsega6658@gmail.com>
2024-10-28 16:57:34 +0300

    data: add skill data to lib

---
+++ src/lib/data.ts
@@ -0,0 +1,44 @@
+import {
+  SiTypescript,
+  SiJavascript,
+  SiNextdotjs,
+  SiPython,
+  SiHtml5,
+  SiCss3,
+  SiGit,
+  SiMysql,
+  SiDocker,
+  SiTailwindcss,
+  SiPandas,
+  SiTensorflow,
+  SiDjango,
+  SiLaravel,
+  SiFlask,
+  SiC,
+  SiGnubash,
+} from "react-icons/si";
+import { BiLogoPostgresql } from "react-icons/bi";
+
+export const languages = [
+  { icon: SiTypescript, text: "TypeScript" },
+  { icon: SiJavascript, text: "JavaScript" },
+  { icon: SiPython, text: "Python" },
+  { icon: SiHtml5, text: "HTML5" },
+  { icon: SiCss3, text: "CSS3" },
+  { icon: SiC, text: "C-programming" },
+  { icon: SiGnubash, text: "Bash" },
+];
+
+export const technologies = [
+  { icon: SiNextdotjs, text: "Next.js" },
+  { icon: SiTailwindcss, text: "Tailwind CSS" },
+  { icon: SiMysql, text: "MySQL" },
+  { icon: SiDjango, text: "Django" },
+  { icon: SiLaravel, text: "Laravel" },
+  { icon: SiFlask, text: "Flask" },
+  { icon: BiLogoPostgresql, text: "PostgreSQL" },
+  { icon: SiPandas, text: "Pandas" },
+  { icon: SiTensorflow, text: "Tensorflow" },
+  { icon: SiGit, text: "Git" },
+  { icon: SiDocker, text: "Docker" },
+];

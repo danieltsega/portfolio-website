6b9a2f09f8a2362f4bf05e55687cc3ebc1aa15f7
danieltsega <danieltsega6658@gmail.com>
2024-10-28 23:01:10 +0300

    feat: add footer to the page and comment out sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Bio from "@/components/bio";
 import Contact from "@/components/contact-section";
+import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import Projects from "@/components/project-section";
 import SkillSection from "@/components/skill-section";
@@ -9,19 +10,29 @@ import Work from "@/components/work-section";
 export default function Home() {
   return (
     <div className="md:max-w-3xl mx-auto md:mt-8">
+      {/*Nav-Bar Section*/}
       <Navbar></Navbar>
+      {/*Hero Section*/}
       <Bio />
       <Separator />
+      {/*Skill Section*/}
       <SkillSection />
+      {/*Project Section*/}
       <section id="projects">
         <Projects />
       </section>
+      {/*Work Section*/}
       <section id="work">
         <Work />
       </section>
+      {/*Contact Section*/}
       <section id="contact">
         <Contact />
       </section>
+      {/*Footer Section*/}
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }

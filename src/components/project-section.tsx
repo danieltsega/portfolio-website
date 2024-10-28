6bc44b4fbaba571dc26094d658cc17f285d0f2a4
danieltsega <danieltsega6658@gmail.com>
2024-10-28 17:55:47 +0300

    fix: remove horizontal padding for larger screen size and make the text start

--- src/components/project-section.tsx
+++ src/components/project-section.tsx
@@ -5,8 +5,8 @@ import { projects } from "@/lib/data";
 
 const Projects: React.FC = () => {
   return (
-    <section className="py-8 px-4">
-      <h1 className="text-3xl font-bold text-center mb-8">Projects</h1>
+    <section className="py-8 px-4 md:px-0">
+      <h1 className="text-3xl font-bold text-start mb-8">Projects</h1>
       <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {projects.map((project, index) => (
           <ProjectCard

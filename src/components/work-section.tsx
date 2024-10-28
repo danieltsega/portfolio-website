dc6c427e0d787f20b7ff52e5db0a11eb7e53fb1f
danieltsega <danieltsega6658@gmail.com>
2024-10-28 22:53:16 +0300

    fix: fix padding y and margin bottom of work section

--- src/components/work-section.tsx
+++ src/components/work-section.tsx
@@ -3,7 +3,7 @@ import { workExperiences } from "@/lib/data";
 
 const Work: React.FC = () => {
   return (
-    <section className="py-8 px-4 md:px-0">
+    <section className="mt-4 px-4 md:px-0">
       <h1 className="text-3xl font-bold text-start mb-8">Work Experience</h1>
       <div>
         {workExperiences.map((work, index) => (

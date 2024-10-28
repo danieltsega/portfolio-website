ca888964ec2dbcef73e2e6120d3577195c82b8d6
danieltsega <danieltsega6658@gmail.com>
2024-10-28 18:26:19 +0300

    feat: add work section component

---
+++ src/components/work-section.tsx
@@ -0,0 +1,25 @@
+import WorkCard from "./work-outline";
+import { workExperiences } from "@/lib/data";
+
+const Work: React.FC = () => {
+  return (
+    <section className="py-8 px-4 md:px-0">
+      <h1 className="text-3xl font-bold text-start mb-8">Work Experience</h1>
+      <div>
+        {workExperiences.map((work, index) => (
+          <WorkCard
+            key={index}
+            logo={work.logo}
+            companyTitle={work.companyTitle}
+            role={work.role}
+            description={work.description}
+            startDate={work.startDate}
+            endDate={work.endDate}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Work;

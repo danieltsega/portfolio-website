4421cbfa574d5b9e9a64f0ee73e5e5a44f4f4497
danieltsega <danieltsega6658@gmail.com>
2024-11-14 13:22:23 +0300

    update: add wrench emojit to the section header

--- src/components/project-section.tsx
+++ src/components/project-section.tsx
@@ -6,7 +6,7 @@ import { projects } from "@/lib/data";
 const Projects: React.FC = () => {
   return (
     <section className="py-8 px-4 md:px-0">
-      <h1 className="text-3xl font-bold text-start mb-8">Projects</h1>
+      <h1 className="text-3xl font-bold text-start mb-8">🛠️Projects</h1>
       <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {projects.map((project, index) => (
           <ProjectCard

a7b9a7bee6e2fc93b0e84a67997d706d3eb1d280
danieltsega <danieltsega6658@gmail.com>
2024-11-14 13:21:54 +0300

    update: add mailbox emoji to the ssection heading

--- src/components/contact-section.tsx
+++ src/components/contact-section.tsx
@@ -3,7 +3,7 @@ import ContactCard from "./contact-card";
 const Contact: React.FC = () => {
   return (
     <section className="py-8 px-4 md:px-0">
-      <h1 className="text-3xl font-bold text-start mb-8">Contact</h1>
+      <h1 className="text-3xl font-bold text-start mb-8">📬Contact</h1>
       <ContactCard />
     </section>
   );

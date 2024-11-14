88680e2d9c6afba15d6288d692d9f489519aee2f
danieltsega <danieltsega6658@gmail.com>
2024-11-14 17:40:18 +0300

    fix: fix icon constant color by providing condition based on the technology

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -4,6 +4,19 @@ import Link from "next/link";
 import { IconType } from "react-icons";
 import { ExternalLink } from "lucide-react";
 
+import {
+  SiTypescript,
+  SiJavascript,
+  SiNextdotjs,
+  SiHtml5,
+  SiCss3,
+  SiTailwindcss,
+  SiDjango,
+  SiLaravel,
+  SiFlask,
+  SiC,
+} from "react-icons/si";
+
 interface ProjectCardProps {
   thumbnail: string;
   title: string;
@@ -35,10 +48,48 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
           {description}
         </p>
         <div className="flex items-center space-x-2 mb-4">
-          {technologies.map((Icon, index) => (
-            <Icon key={index} className="w-5 h-5 text-blue-500" />
-          ))}
+          {technologies.map((Icon, index) => {
+            let iconColor = "text-blue-500"; // default color
+
+            switch (Icon) {
+              case SiC:
+                iconColor = "text-black";
+                break;
+              case SiNextdotjs:
+                iconColor = "text-black";
+                break;
+              case SiTailwindcss:
+                iconColor = "text-blue-500";
+                break;
+              case SiHtml5:
+                iconColor = "text-orange-500";
+                break;
+              case SiCss3:
+                iconColor = "text-blue-500";
+                break;
+              case SiJavascript:
+                iconColor = "text-yellow-500";
+                break;
+              case SiTypescript:
+                iconColor = "text-blue-500";
+                break;
+              case SiDjango:
+                iconColor = "text-green-500";
+                break;
+              case SiLaravel:
+                iconColor = "text-red-500";
+                break;
+              case SiFlask:
+                iconColor = "text-green-500";
+                break;
+              default:
+                break;
+            }
+
+            return <Icon key={index} className={`w-5 h-5 ${iconColor}`} />;
+          })}
         </div>
+
         <div className="flex"></div>
       </div>
     </div>

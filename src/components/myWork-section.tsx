cb201ba8f18a12773905e889e8e530f494bd9455
danieltsega <danieltsega6658@gmail.com>
2025-07-26 10:07:02 -0400

    feat: add my work section and import the outline to it

---
+++ src/components/myWork-section.tsx
@@ -0,0 +1,27 @@
+import MyWorkCard from "./myWork-outline";
+import { myWorks } from "@/lib/data";
+
+const MyWork: React.FC = () => {
+  return (
+    <section className="mt-4 px-4 md:px-0">
+      <h1 className="text-3xl font-bold text-start mb-8">
+        💼Work Experience & Internships
+      </h1>
+      <div>
+        {myWorks.map((work, index) => (
+          <MyWorkCard
+            key={index}
+            logo={work.logo}
+            companyName={work.companyName}
+            associatedCompany={work.associatedCompany}
+            description={work.description}
+            technologies={work.technologies}
+            liveLink={work.liveLink}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default MyWork;

0e1afb2ebdf73959879b4e0ca4642b44f5a78c20
danieltsega <danieltsega6658@gmail.com>
2025-07-26 15:53:21 -0400

    fix: fixed the style of bio section

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -7,14 +7,14 @@ const Bio = () => {
   return (
     <section
       id="bio"
-      className="my-10 p-4 flex flex-col md:flex-row items-center justify-center"
+      className="md:relative md:mb-44 my-10 shadow-lg border rounded-lg p-4 flex flex-col gap-2 md:flex-row items-center justify-center"
     >
       <div className="w-32 h-32 md:w-1/3 md:h-auto md:rounded-none rounded-full overflow-hidden">
         <Image
           src={developerPhoto}
           alt="Developer Photo"
           layout="responsive"
-          className="object-cover"
+          className="object-cover rounded-full border"
         />
       </div>
       <div className="w-full md:w-2/3 md:pl-4 mt-8 md:mt-0 text-center md:text-left">
@@ -29,14 +29,10 @@ const Bio = () => {
               👋
             </span>
           </span>
-          <br /> am Daniel Tsega
+          <br /> am Danny
         </h2>
-        <p className="text-lg text-muted-foreground tracking-tight">
-          Am a dedicated developer specializing in{" "}
-          <span className="font-bold">Next.js</span> for front-end and
-          <span className="font-bold"> Django </span>
-          for back-end, with past experience in Laravel. Currently, am eager to learn about AI, Software security & Low level programming, while also nurturing a deep interest in computer architectures
-          and low-level computing concepts.
+        <p className="md:absolute bg-gray-50 top-44 text-lg p-4 shadow-md border rounded-lg text-muted-foreground tracking-tight">
+          I've always been drawn to how computers evolved — from early machines to the birth of programming. That curiosity turned into a passion for building things with code. I enjoy solving problems, understanding how tools work under the hood, and crafting clean, efficient systems that actually make a difference.
         </p>
       </div>
     </section>
